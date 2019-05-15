dd991a237a56b09f1969a7fd076a6e927cbb59ac
mi-24v <miwpayou0808@gmail.com>
2019-05-15 21:42:41 +0900

    initial project

---
+++ app/src/main/java/mipwayou0808/info/overfrontlay/MainActivity.kt
@@ -0,0 +1,12 @@
+package mipwayou0808.info.overfrontlay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
